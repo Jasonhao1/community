c09eb02729019f4f83c1ae4281a37dc402c1300e
Jasonhao1 <84440610+Jasonhao1@users.noreply.github.com>
2022-05-10 21:10:38 +0800

    登录

---
+++ src/main/java/com/nowcode/community/dao/LoginTicketMapper.java
@@ -0,0 +1,37 @@
+package com.nowcode.community.dao;
+
+import com.nowcode.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * Created by Intellij IDEA.
+ * User:  haose
+ * Date:  2022/05/10
+ */
+@Mapper
+public interface LoginTicketMapper {
+
+    @Insert({
+            "insert into login_ticket(user_id,ticket,status,expired) ",
+            "values(#{userId},#{ticket},#{status},#{expired}) "
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    @Select({
+            "select id,user_id,ticket,status,expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({
+            "<script> ",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\"> ",
+            "and 1=1 ",
+            "</if> ",
+            "</script>"
+    })
+    int updateStatus(String ticket,int status);
+
+}
