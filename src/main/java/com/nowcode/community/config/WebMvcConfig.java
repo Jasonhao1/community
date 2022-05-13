14c87894c6a31d040e9e081ced3a5f04996503b8
Jasonhao1 <84440610+Jasonhao1@users.noreply.github.com>
2022-05-13 15:35:26 +0800

    显示登录信息

--- src/main/java/com/nowcode/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcode/community/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.nowcode.community.config;
 
 import com.nowcode.community.controller.interceptor.AlphaInterceptor;
+import com.nowcode.community.controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -18,13 +19,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private AlphaInterceptor alphaInterceptor;
     
-    
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg")
                 .addPathPatterns("/regidter","/login");
+
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
     }
 
 }
